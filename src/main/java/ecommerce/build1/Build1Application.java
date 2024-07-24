09c407accdb06ba3dd3ae49e105e8eea19fabef8
ikhwn99 <115068702+ikhwn99@users.noreply.github.com>
2024-07-24 23:29:54 +0800

    Spring Initialized

---
+++ src/main/java/ecommerce/build1/Build1Application.java
@@ -0,0 +1,13 @@
+package ecommerce.build1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Build1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Build1Application.class, args);
+	}
+
+}
